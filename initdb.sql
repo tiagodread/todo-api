12340dcca298a8afa51b2e28cc23d60cc3bbe3a3
Tiago Góes <tiago.goes2009@gmail.com>
2024-08-08 11:00:09 -0300

    Moving files around and fixing more stuff
    Signed-off-by: Tiago Góes <tiago.goes2009@gmail.com>

---
+++ initdb.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS public;
+
+
+CREATE TABLE IF NOT EXISTS public.task (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	description text,
+	created_at DATE NOT NULL,
+	completed_at DATE,
+	is_completed bool NOT NULL,
+	reward_in_sats integer NOT NULL
+);
